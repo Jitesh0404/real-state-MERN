2683be432187957cc37829a452fb1476db3963df
Jitesh Sharma <jitesh04sharma@gmail.com>
2023-11-29 12:45:18 +0530

    Setup the Database Connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
+dotenv.config();
 const app = express();
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to DB");
+}).catch(()=>{
+    console.log("Opps!! Could not connect to DB");
+})
 
 
 app.listen(3001,()=>{

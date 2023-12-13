51ff05e2775bbe85f53cd5a331b13704c6d2396b
Jitesh0404 <jitesh04sharma@gmail.com>
2023-12-13 21:34:04 +0530

    Created First test route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv')
+const userRouter = require("./routes/userRoute");
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to DB");
-}).catch(()=>{
+  })
+  .catch(() => {
     console.log("Opps!! Could not connect to DB");
-})
+  });
 
+app.use("/api/user", userRouter);
 
-app.listen(3001,()=>{
-    console.log("Server running on port : 3000");
-})
+app.listen(3001, () => {
+  console.log("Server running on port : 3001");
+});

e450225fd20ad8583353a7c2cb71666d95aaa374
Jitesh0404 <jitesh04sharma@gmail.com>
2023-12-14 21:14:55 +0530

    create error handler middleware and error function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,17 @@
-const {User} = require("../models/user.model.js");
-const bcrypt = require('bcrypt');
-const signup = async(req,res,next)=>{
-    const {username,email,password} = req.body;
-    const hashedPassword = bcrypt.hashSync(password,10);
-    const newUser = new User({username,email,password:hashedPassword});
-    try {
-        await newUser.save();
-        res.status(201).json("User Created Successfully !!");
-        
-    } catch (error) {
-        next(error)
-    }
-}
+const { User } = require("../models/user.model.js");
+const bcrypt = require("bcrypt");
+const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User Created Successfully !!");
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports={
-    signup
-}
+module.exports = {
+  signup,
+};

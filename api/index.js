3c83d0b070eb6b4aadbb6ab4ccae49ed4921f69b
Jitesh0404 <jitesh04sharma@gmail.com>
2023-12-14 14:19:43 +0530

    created singup api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoute");
+const authRouter = require("./routes/auth.route")
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-
+app.use(express.json())
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -14,8 +15,8 @@ mongoose
     console.log("Opps!! Could not connect to DB");
   });
 
-app.use("/api/user", userRouter);
-
 app.listen(3001, () => {
   console.log("Server running on port : 3001");
 });
+app.use("/api/user", userRouter);
+app.use("/api/auth",authRouter)
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -22,4 +22,6 @@ const userSchema = mongoose.Schema(
 
 const User = mongoose.model('User',userSchema);
 
-export default User;
+module.exports={
+  User
+}

cfa72adf46cd2619ff01a6b425e56fb89d90c0fa
Jitesh0404 <jitesh04sharma@gmail.com>
2024-02-03 12:52:58 +0530

    created update listing route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createListing ,deleteListing} = require('../controllers/listing.controller.js');
+const { createListing ,deleteListing,updateListing} = require('../controllers/listing.controller.js');
 const { verifyToken } = require('../utils/verifyUser.js');
 
 router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
+router.post('/update/:id',verifyToken,updateListing);
 
 module.exports = router
